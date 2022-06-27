b1ae3693c11ca0b5ef28e3d0dd179d72171de8bb
Kate <koitloit5@gmail.com>
2022-06-27 18:02:20 +0300

    shop-card

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,8 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICartProduct } from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
 	selector: 'ngx-shop-card',
 	templateUrl: './shop-card.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopCardComponent {}
+export class ShopCardComponent {
+	@Input() product: ICartProduct = {} as ICartProduct;
+
+	@Output()
+	public increment = new EventEmitter();
+
+	@Output()
+	public decrement = new EventEmitter();
+
+	public incrementProductInCart(): void {
+		this.increment.emit();
+	}
+
+	public decrementProductInCart(): void {
+		this.decrement.emit();
+	}
+}
